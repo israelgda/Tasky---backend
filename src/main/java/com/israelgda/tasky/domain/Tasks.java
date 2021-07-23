cb70e19e7e96c699b118982a39d50592944f734e
Israel Gadelha <gdelha.israel@gmail.com>
2021-07-23 19:46:34 -0300

    Criação da entidade Tasks

---
+++ src/main/java/com/israelgda/tasky/domain/Tasks.java
@@ -0,0 +1,104 @@
+package com.israelgda.tasky.domain;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Tasks implements Serializable{
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	
+	private String titulo;
+	private String descricao;
+	private Date dataFinal;
+	private boolean finalizado;
+	
+	//Construtores
+	public Tasks() {
+		super();
+	}
+	
+	public Tasks(Integer id, String titulo, String descricao, Date dataFinal, boolean finalizado) {
+		super();
+		this.id = id;
+		this.titulo = titulo;
+		this.descricao = descricao;
+		this.dataFinal = dataFinal;
+		this.finalizado = finalizado;
+	}
+	
+	//Getters and Setters
+	public Integer getId() {
+		return id;
+	}
+	
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	
+	public String getTitulo() {
+		return titulo;
+	}
+	
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+	
+	public String getDescricao() {
+		return descricao;
+	}
+	
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	
+	public Date getDataFinal() {
+		return dataFinal;
+	}
+	
+	public void setDataFinal(Date dataFinal) {
+		this.dataFinal = dataFinal;
+	}
+	
+	public boolean isFinalizado() {
+		return finalizado;
+	}
+	
+	public void setFinalizado(boolean finalizado) {
+		this.finalizado = finalizado;
+	}
+	
+	//Hashcode and Equals
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Tasks other = (Tasks) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+
+}
