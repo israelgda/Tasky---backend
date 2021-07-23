421d352943f26cd9f1933f1043d718c63e27d07c
Israel Gadelha <gdelha.israel@gmail.com>
2021-07-23 19:40:38 -0300

    Projeto criado

---
+++ src/main/java/com/israelgda/tasky/TaskyappApplication.java
@@ -0,0 +1,13 @@
+package com.israelgda.tasky;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskyappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskyappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/israelgda/tasky/TaskyappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.israelgda.tasky;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskyappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
