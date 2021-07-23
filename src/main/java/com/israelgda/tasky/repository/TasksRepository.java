67bbb74b318a6a02fd6c6ef5ce0277f46f14dafc
Israel Gadelha <gdelha.israel@gmail.com>
2021-07-23 19:55:26 -0300

    Criação do TasksRepository e instanciação incial de teste no H2

---
+++ src/main/java/com/israelgda/tasky/repository/TasksRepository.java
@@ -0,0 +1,11 @@
+package com.israelgda.tasky.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.israelgda.tasky.domain.Tasks;
+
+@Repository
+public interface TasksRepository extends JpaRepository<Tasks, Integer> {
+
+}
